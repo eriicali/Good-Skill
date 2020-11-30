3707591fa9db77f106698a0a0f68a9399baf2cb3
preethiN12 <preethi.nagabandi2002@gmail.com>
2020-11-29 19:02:49 -0500

    add drinks

---
+++ src/Drinks/Alcohol.java
@@ -0,0 +1,10 @@
+package Drinks;
+public class Alcohol extends Beverage{
+    public Alcohol() {
+        super(-5,-5,-5);
+    }
+    public String toString() {
+        super.toString();
+        return "Alcohol";
+    }
+}
---
+++ src/Drinks/Beverage.java
@@ -0,0 +1,20 @@
+package Drinks;
+public abstract class Beverage {
+    protected int healthChange;
+    protected int energyChange;
+    protected int speedChange;
+
+    public Beverage(int healthChange, int energyChange, int speedChange) {
+        this.healthChange = healthChange;
+        this.energyChange = energyChange;
+        this.speedChange = speedChange;
+    }
+
+    public String toString() {
+        return "Beverage{" +
+                "healthChange=" + healthChange +
+                ", energyChange=" + energyChange +
+                ", speedChange=" + speedChange +
+                '}';
+    }
+}
