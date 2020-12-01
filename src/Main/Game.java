5d20444d29b6e21ffbf6701a43000d87be4efa68
dokzai <38355190+dokzai@users.noreply.github.com>
2020-11-30 21:59:52 -0500

    2nd try

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -23,23 +23,21 @@ public class Game extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        primaryStage.setTitle("Hello preethi");
         StackPane root = new StackPane();
-        Scene scene = new Scene(root, 640, 480);
         Image test = new Image("file:../../resources/menubg.gif");
         BackgroundImage bgImg = new BackgroundImage(test,
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.DEFAULT,
                 new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false));
-
+        root.setBackground(new Background(bgImg));
+        Scene scene = new Scene(root, 640, 480);
 // put stuff in root as normal....
-        ImagePattern pattern = new ImagePattern(test);
-        scene.setFill(pattern);
+
         primaryStage.setScene(scene);
         primaryStage.show();
 
 
-        primaryStage.show();
+
 
         //make stuff (buttons, etc.)
         //add that stuff to a scene
