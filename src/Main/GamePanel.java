a86961f93b05e52ba790f0cfd61d4241ca95bae6
dokzai <38355190+dokzai@users.noreply.github.com>
2020-12-01 20:37:49 -0500

    wtf

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -79,7 +79,9 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
     }
     private void drawToScreen() {
         Graphics g2 = getGraphics();
-        g2.drawImage(image, 0, 0, null);
+        g2.drawImage(image, 0, 0,
+                WIDTH *SCALE, HEIGHT*SCALE,
+                null);
         g2.dispose();
     }
 
