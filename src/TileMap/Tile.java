5ea14d1023bed73b45e541e23b0520a67033eba0
dokzai <38355190+dokzai@users.noreply.github.com>
2020-12-13 18:35:23 -0500

    added comments to tilemap and main packages

--- src/TileMap/Tile.java
+++ src/TileMap/Tile.java
@@ -1,25 +1,27 @@
 package TileMap;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImageFilter;
 
 public class Tile {
     private BufferedImage image;
     private int type;
-    
-    // tile types
-    //player can go through
+
+    // tile player can traverse through
     public static final int NORMAL = 0;
-    //player can't go throught
+    // tile player cannot traverse through
     public static final int BLOCKED = 1;
-    
+
+    // constructor of tile
     public Tile(BufferedImage image, int type){
         this.image = image;
         this.type = type;
     }
+    // returns type of tile
     public int getType(){
         return this.type;
     }
+
+    // returns image of tile
     public BufferedImage getImage(){
         return this.image;
     }
