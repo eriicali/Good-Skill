c31d487c24df64c78f75e8314c2fec4422916aa5
dokzai <38355190+dokzai@users.noreply.github.com>
2020-12-13 18:35:51 -0500

    Revert "Revert "added comments to tilemap and main packages""
    This reverts commit 5452f0ddaec833c082d6c3ff73297306b92f97cf.

--- src/TileMap/Background.java
+++ src/TileMap/Background.java
@@ -5,38 +5,45 @@ import java.awt.image.*;
 import javax.imageio.ImageIO;
 
 public class Background {
+    //init variables
     private BufferedImage image;
     private double x;
     private double y;
     private double dx;
     private double dy;
-
     private double moveScale;
+
+    // constructor
     public Background(String s, double ms) {
+        // reads image as a resource stream and sets move scale
         try {
             image = ImageIO.read(
                 getClass().getResourceAsStream(s)
             );
             moveScale = ms;
         }
+        // catches exception
         catch (Exception e) {
             e.printStackTrace();
         }
     }
+    // sets position of background when updates in level (aids with parallax effect)
     public void setPosition(double x, double y) {
         this.x = (x * moveScale) % GamePanel.WIDTH;
         this.y = (y * moveScale) % GamePanel.HEIGHT;
     }
-    public void setVector(double dx, double dy) {
-        this.dx = dx;
-        this.dy = dy;
-    }
+    // updates moving vector (scale)
     public void update() {
         x += dx;
         y += dy;
     }
+
+    // draws images for game states
+    // draws parallax effect
     public void draw(Graphics2D g) {
+        //draw image
         g.drawImage(image, (int)x,(int)y,null);
+        // if x is smaller than 0 or smaller than 0, draw the image (cut off part) and the rest accordingly
         if(x < 0) {
             g.drawImage(image, (int)x+GamePanel.WIDTH, (int)y, null);
         }
--- src/TileMap/TileMap.java
+++ src/TileMap/TileMap.java
@@ -1,5 +1,4 @@
 package TileMap;
-import GameState.GameStateManager;
 import Main.GamePanel;
 
 import java.awt.*;
@@ -15,14 +14,16 @@ public class TileMap
     private double x;
     private double y;
     
-    // bounds
+    // domain/range bounds
     private int xmin;
     private int ymin;
     private int xmax;
     private int ymax;
-    
+
+    // animation tween of map
     private double tween;
-    // map
+
+    // map variables
     private int[][] map;
     private int tileSize;
     private int numRows;
@@ -30,64 +31,83 @@ public class TileMap
     private int width;
     private int height;
     
-    //tileset
+    // tileset
     private BufferedImage tileset;
     private int numTilesAcross;
     private Tile[][] tiles;
     
-    //drawing
+    // drawing
     private int rowOffset;
     private int colOffset;
     private int numRowsToDraw;
     private int numColsToDraw;
-    
+
+    // constructor
     public TileMap(int tileSize)
     {
+        // initializes variables
         this.tileSize = tileSize;
+        // rows to draw
         numRowsToDraw = GamePanel.HEIGHT / tileSize + 2;
         numColsToDraw = GamePanel.WIDTH / tileSize + 2;
+        // sets tween
         tween = 0.07;
     }
-    
+
+    //loads tiles
     public void loadTiles(String s)
     {
         try
         {
+            // loads tile set from resource stream
             tileset = ImageIO.read(getClass().getResourceAsStream(s));
             numTilesAcross = tileset.getWidth() / tileSize;
             tiles = new Tile[2][numTilesAcross];
+            // init subimages (for cutting the tileset images)
             BufferedImage subimage;
+            // initializes blocked and normal (traversable) tiles
             for (int col = 0; col < numTilesAcross; col++)
             {
+                // creates sub-image of normal and creates a normal tile
                 subimage = tileset.getSubimage(col * tileSize, 0, tileSize, tileSize);
                 tiles[0][col] = new Tile(subimage, Tile.NORMAL);
+                // creates sub-image of normal and creates a blocked tile tile
                 subimage = tileset.getSubimage(col * tileSize, tileSize, tileSize, tileSize);
                 tiles[1][col] = new Tile(subimage, Tile.BLOCKED);
             }
-        } catch (Exception e)
+        }
+        // catches exceptions and prints stack trace in case of exceptions
+        catch (Exception e)
         {
             e.printStackTrace();
         }
     }
-    
+
+    // loads map on a level state
     public void loadMap(String s)
     {
+        // reads from tilemap file
         try
         {
             //read from the tilemap file
             //load map to the window
             InputStream in = getClass().getResourceAsStream(s);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            // wrap integer
             numCols = Integer.parseInt(br.readLine());
             numRows = Integer.parseInt(br.readLine());
+            // initializes map 2d array
             map = new int[numRows][numCols];
+            // size of map in terms of pixels (per tile size)
             width = numCols * tileSize;
             height = numRows * tileSize;
+            // inits the x min and max (for camera) of the tile map
             xmin = GamePanel.WIDTH - width;
             xmax = 0;
             ymin = GamePanel.HEIGHT - height;
             ymax = 0;
-            // this is a regex delimiter for whitespace
+            // this is a regex delimiter for whitespace in the .map text file (it's essentially a text file)
+            // and basically translates and parses it into the 2d array tile map
             String delims = "\\s+";
             for (int row = 0; row < numRows; row++)
             {
@@ -98,91 +118,101 @@ public class TileMap
                     map[row][col] = Integer.parseInt(tokens[col]);
                 }
             }
-    
-        } catch (Exception e)
+        }
+        //catches resource reading exceptions and shows stack trace
+        catch (Exception e)
         {
             e.printStackTrace();
         }
     }
-    
+    // returns tile size
     public int getTileSize()
     {
         return tileSize;
     }
-    
+
+    //returns x of a tile for setting map position of entities
     public double getX()
     {
         return x;
     }
-    
+
+    //returns y of a tile for setting map position of entities
     public double getY()
     {
         return y;
     }
-    
+
+    //returns width of entire tile map
     public int getWidth()
     {
         return width;
     }
-    
+
+    //returns height of entire tilemap
     public int getHeight()
     {
         return height;
     }
-    
+
+    // returns type of tile (blocked or traversable)
     public int getType(int row, int col)
-    {//returns type of tiles normal/blocked
+    {
         int rc = map[row][col];
         int r = rc / numTilesAcross;
         int c = rc % numTilesAcross;
         return tiles[r][c].getType();
     }
 
-    //gradual shift between different animations, smoother visuals
+    // smooth shift between different animations
     public void setTween(double d)
     {
         tween = d;
     }
-    
+
+    //changes/sets position of tilemaps as game progresses
     public void setPosition(double x, double y)
     {
-        //change position of tilemap as game progresses
+        // tween is used here to make the transition smoother
         this.x += (x - this.x) * tween;
         this.y += (y - this.y) * tween;
+        // fixes the bounds of the tilemap/camera
         fixBounds();
         colOffset = (int) -this.x / tileSize;
         rowOffset = (int) -this.y / tileSize;
     }
-    
+
+    // fixes bounds for tilemap (when player is moving) and allows for tiles to be set accordingly (for camera)
     private void fixBounds()
     {
+        // sets xmin/max and ymin/max
         if (x < xmin) x = xmin;
         if (y < ymin) y = ymin;
         if (x > xmax) x = xmax;
         if (y > ymax) y = ymax;
     }
-    
+
+    // draws tilemap accordingly
     public void draw(Graphics2D g)
     {
+        // for every row in the tile map (shown to player)
         for (int row = rowOffset; row < rowOffset + numRowsToDraw; row++)
         {
             if (row >= numRows) break;
+            // for every col in the tile map (shown to player)
             for (int col = colOffset; col < colOffset + numColsToDraw; col++)
             {
+                // break if column is bigger than the number of columns needed
                 if (col >= numCols) break;
+                // if the tile is an open traversable block for the player a "0" in the .map text file, then continue and don't draw the tile
                 if (map[row][col] == 0) continue;
                 int rc = map[row][col];
                 int r = rc / numTilesAcross;
                 int c = rc % numTilesAcross;
+                // draw tile/image at that place
                 g.drawImage(tiles[r][c].getImage(), (int) x + col * tileSize, (int) y + row * tileSize, null);
             }
         }
     }
-    
-    public int getNumRows() {
-        return numRows;
-    }
-    public int getNumCols() {
-        return numCols;
-    }
+
 }
